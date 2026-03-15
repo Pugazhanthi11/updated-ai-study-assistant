4ca3fe3d6fcb1413b501cff73b9309b46e6dfa39
Pugazhanthi11 <pugazhanthi.vk@gmail.com>
2026-03-15 08:19:25 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -456,12 +456,6 @@ window.location.href="login.html"
 
 }
 
-/* ================= MOBILE SIDEBAR ================= */
-
-function toggleSidebar(){
-document.getElementById("sidebar").classList.toggle("active")
-}
-
 loadHistory()
 
 addStreamingMessage(
